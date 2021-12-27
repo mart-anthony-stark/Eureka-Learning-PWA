0a315677bf214c33776fd5afa4ef2cd7b3b399a0
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-27 14:27:48 +0800

    Add app layout

---
+++ Frontend-NUXT/layouts/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt />
+    <Navbar />
+  </div>
+</template>
+
+<style>
+section {
+  min-height: calc(100vh - 50px);
+}
+#bg {
+  position: fixed;
+  z-index: -1;
+}
+</style>
