8e9cd345ac99a1cbfe7cfc78b378659485bef7fd
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-27 14:28:57 +0800

    Add chemistry page

---
+++ Frontend-NUXT/pages/chemistry.vue
@@ -0,0 +1,303 @@
+<template>
+  <div class="body">
+    <NavHeader />
+    <ChapterHeader :chapter="intro">
+      <Slider :pics="pics" />
+    </ChapterHeader>
+    <div class="topic_section" id="topic_1">
+
+	<div class="topic_block" id="page_greeting">
+
+		<h1>MATTER</h1>
+
+		<iframe 
+		src="https://www.youtube.com/embed/ELchwUIlWa8">
+	</iframe>
+
+		<p>Matter is <b>everything</b>. Matter is what we always see and feel. 
+		It can be solid, liquid and gas. How to identify each and one of them with their properties? 
+		Let's all find out!!!</p>
+
+	</div>
+
+	<div class="topic_block" id="">
+		
+		<h1 style="text-transform: uppercase;">3 States of Matter</h1>
+
+		<iframe 
+			src="https://www.youtube.com/embed/wclY8F-UoTE">
+		</iframe>
+
+		<h2>
+		<p>
+			<ol>
+				<li>SOLID</li>
+				<img src="/images/chemistry/solid.png">
+
+				<ul>
+					<li>It keeps the volume of particles trapped in the surroundings consistent and enables them to sustainably grow on a strong basis.</li>
+					<li>Amongst particles, there may be a finite quantity of unfastened area. This is typically flexible.</li>
+					<li>There may be no way to separate stable particles. They are not doing so properly now.</li>
+				</ul>
+				
+				<br>
+
+				<li>LIQUID</li>
+				<img src="/images/chemistry/liquid.png">
+
+				<ul>
+					<li>The vital elements are acquired by particles as they move from one area to the next.</li>
+					<li>For all round particles that do not twist quickly, there may be a small empty area.</li>
+					<li>When the motion is rapid, particles can collide.</li>
+				</ul>
+				
+				<br>
+				
+				<li>GAS</li>
+				<img src="/images/chemistry/gas.png">
+
+				<ul>
+					<li>The particles need to be able to bypass each other because of the form and volume of the containment.</li>
+					<li>When the particles are compressed together, there is lots of vacant space between them.</li>
+					<li>If the waft is almost identical, the particles can quite simply move over each other.</li>
+				</ul>
+			</ol>
+		</p>
+		</h2>
+	
+	</div>
+</div>
+
+<div class="topic_section" id="topic_2">
+
+	<div class="topic_block">
+
+		<h1>ATOMS AND ELEMENTS</h1><br>
+
+		<h3>ATOM OR ELEMENT ?</h3>
+
+		<iframe 
+			src="https://www.youtube.com/embed/M-1nzFZGaAM">
+		</iframe>
+
+		<h2>ATOM</h2>
+
+		<img src="/images/chemistry/atom.png">
+
+		<p>
+			This atom is the smallest particle of a substance that preserves all of an element's chemical composition. 
+			The nucleus, which would be in the heart of the atom and comprise neutrons and protons, and the outside portion of the atom that maintains the atom's electrons in circular orbits, 
+			make up an atom. Protons, electrons, and neutrons, amongst many other fundamental particles, make up atoms. 
+			The lonely passing over is hydrogen "H", which consists of a proton and an electron but, under no circumstances, neutrons.
+		</p>
+
+		<p>
+			Neutrons, unlike protons, just don't have a straight electrical current. 
+			As a result, while the unit weight of an atom is affected by the number of neutrons in it, the electric field is unaffected. 
+			Protons are more complicated than electrons, and the capabilities of protons and electrons inside every atom of each element varies. 
+			The quantity of protons in an element determines its electronic configuration, which is used to differentiate it from all other elements.
+
+			<br><br>
+					<b>Protons</b>===<b>Neutrons</b>===<b>Electrons</b><br> 
+					Charge=====Mass=====Location<br>
+	 				+1=========1========nucleus<br>
+	   				0==========1========nucleus<br>
+	 				–1=========0========orbitals<br>
+
+		</p>
+
+		<h2>ELEMENTS</h2>
+
+		<img src="/images/chemistry/ptable.png">
+
+		<p>	
+			Life is composed of matter at its most foundational level. 
+			Whatever component has weight and the ability to occupy space, it is considered matter. 
+			Elements are distinct types of matter with special physicochemical characteristics that 
+			cannot be split into different substances by regular physicochemical processes. The chemical 
+			symbol for each element is a unique capital letter or a mixture of multiple letters if the 
+			first term itself is "owned" by some other element.
+		</p>
+
+		<video width="530" height="380" autoplay muted>
+			<source src="/images/chemistry/Chemistryelements.mp4" type="video/mp4">
+		</video>
+
+	</div>
+</div>
+
+<div class="topic_section" id="topic_3">
+	
+	<div class="topic_block">
+		
+		<h1>ION</h1>
+
+		<iframe 
+			src="https://www.youtube.com/embed/zTUnjPALX_U">
+		</iframe>
+
+		<h2>ALL ABOUT ION!</h2>
+
+		<div class="two_column">
+			<p>
+				Ions are produced while the wide variety of protons in an atom is greater than the quantity of electrons. 
+				While there are more protons than electrons, ions are referred to as cation ions, and when there are greater electrons, ions are called anions. 
+				Considering the fact that they have got extra protons than electrons, the cation is undoubtedly positively charged. 
+				So that they have more electrons than protons. Anion ions are obviously negatively charged.
+			</p>
+			<img src="/images/chemistry/ions.png">
+		</div>
+		
+	</div>
+
+	<div class="topic_section" id="topic_4">
+
+		<div class="topic_block">
+			<h1>THE MOLECULES AND COMPOUNDS</h1>
+
+			<h2>MOLECULES</h2>
+
+			<img src="/images/chemistry/molecules.png">
+
+			<p>
+				 molecule is formed when an atom interacts with other atoms of the same or various varieties. 
+				Molecules are so small that they are completely invisible. 
+				Scientific researchers, on the other hand, could tear things down to their underlying atomic components. 
+				Molecules include oxygen ("O2) water, which is comprised of two hydrogen atoms and one oxygen atom (H2O).
+			</p>
+
+			<h2>COMPOUNDS</h2>
+
+			<img src="/images/chemistry/compounds.png">
+
+			<p>
+				While atoms of different elements integrate, a compound is fashioned. 
+				But the one component ought to be separate if compounding is required. 
+				For instance, carbon dioxide (CO2) is a compound due to the fact that it is made of specific factors. 
+				Molecules and compounds: What are the differences?
+			</p>
+
+			<p>
+				Molecules are made up of two or more atoms. 
+				They bind the structures of the atoms to each other and keep the bond between them. 
+				People can't see. Compounds, alternatively, encompass one or more specific elements bonded 
+				collectively to form a complete structure. Humans can definitely see.
+			</p>
+		</div>
+	
+
+			<div class="topic_section" id="topic_5">
+	
+				<div class="topic_block">
+					
+					<h1>SOLUTIONS AND MIXTURES</h1>
+			
+					<iframe 
+						src="https://www.youtube.com/embed/_Tck943uH2o">
+					</iframe>
+			
+					<h2>SOLUTIONS!</h2>
+			
+					<div class="two_column">
+						<p>
+							A solution may be an undiversified mixture of particles. 
+							An answer consists of two components: a matter and a solvent. 
+							A solute is a solvent in an exceedingly solvent state. 
+							The solute value of a solute in a solvent is extremely small. 
+							Dissolved substances dissolve in solvents that are marine compounds. 
+							The solvent incorporates an abundant volume greater than the solute. 
+							Solutions may be in liquid or vaporous form.
+						</p>
+						<img src="/images/chemistry/solutions.png">
+					</div>
+
+					<h2>MIXTURES!</h2>
+			
+					<div class="two_column">
+						<p>
+							In chemistry, a mixture is defined as when two or more compounds 
+							combine without causing any chemical change. When the constituents of a 
+							mixture come together, neither their separate properties nor their chemical 
+							are altered. In most cases, they are the outcome of a chemical reaction involving 
+							several compounds or components.
+						</p>
+						
+						<img src="/images/chemistry/mixture.png">
+					</div>
+
+					<p>Mixtures can be divided into two categories:
+						<ul>
+							<li>A heterogeneous mixture is created when two different types of substances with 
+								varied individual properties are combined. For instance, combining salt and sand.</li>
+							<li>A homogeneous mixture is formed when two chemicals with comparable properties are 
+								put together uniformly. For instance, consider the combination of salt and water.</li>	
+						</ul>
+					</p>
+		
+					<h2>THERE'S MORE</h2>
+
+					<p>There's so much to learn about our home and it companion that we couldn't possibly cover it all, so to send you off is once again our friends from Kurzgesagt!</p>
+
+					<iframe 
+						src="https://www.youtube.com/embed/JGXi_9A__Vc">
+					</iframe>
+				</div>
+			</div>
+	</div>
+</div>
+    <EndChapter currentChapter="CHEMISTRY" nextChapter="physics" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      intro: {
+        no: 4,
+        title: 'CHEMISTRY',
+        intro:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.',
+      },
+      pics: [
+        '/images/chemistry/chem.jpg',
+        '/images/chemistry/chemical_soup.jpg',
+        '/images/chemistry/vents.jpg',
+        '/images/chemistry/water.jpg',
+        '/images/chemistry/chem.jpg',
+      ],
+    }
+  },
+  computed: {
+    mode() {
+      return this.$store.state.mode.mode
+    },
+  },
+  mounted() {
+    if (this.mode == '') this.$router.push('/get-started')
+  },
+}
+</script>
+
+<style scoped>
+.topic_block p {
+  font-size: 15px;
+  font-family: RoobertLight;
+  margin-top: 5%;
+  align-items: center;
+  display: flex;
+  max-width: 700px;
+  justify-content: center;
+  flex-direction: column;
+}
+.two_column {
+  width: 100%;
+  padding: 15px;
+  background-color: black;
+  padding-top: 5%;
+  padding-bottom: 5%;
+  color: white;
+  display: flex;
+  justify-content: center;
+}
+</style>
