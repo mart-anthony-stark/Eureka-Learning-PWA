868cbb5e6b9ab6b6c52c7e150d90c9d692a57c34
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-27 14:26:38 +0800

    Add error modal component

---
+++ Frontend-NUXT/components/ErrorModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal">
+    <h3>{{ err || 'Error' }}</h3>
+    <button @click="$emit('closeModal')">Okay</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['err'],
+}
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 999;
+  max-width: 300px;
+  width: 100%;
+  min-height: 200px;
+  padding: 2rem;
+  background: rgb(167, 167, 167);
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+button {
+  margin-top: 4%;
+  width: 100%;
+  height: 40px;
+  background-color: rgb(249, 249, 0);
+  border-style: none;
+  cursor: pointer;
+  justify-self: flex-end;
+}
+h3 {
+  text-align: center;
+}
+</style>
